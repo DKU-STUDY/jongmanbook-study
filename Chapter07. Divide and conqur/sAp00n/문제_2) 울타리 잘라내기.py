e3aac47e17f7da333faaf093e191b3080cc8389f
sAp00n <won7963kr@gmail.com>
2021-01-14 11:17:17 +0900

    Resolve 문제 2) 울타리 잘라내기

--- Chapter07. Divide and conqur/sAp00n/문제_2) 울타리 잘라내기.py
+++ Chapter07. Divide and conqur/sAp00n/문제_2) 울타리 잘라내기.py
@@ -7,39 +7,46 @@ N = 2 * 10^4
 
 하지만.... O(N!*N) 밖에 생각나지 않는다..
 
+-- 이해 완료 --
+
+이해 후에 해답 안보고 다시 코드 작성하다가 머리 터질뻔함;;
+idx의 오른쪽만 열린방향으로 코드를 작성하기가 왤캐 힘드냐
+
 """
 
 from sys import stdin
 
 
-def computed_max(list_of_fence, left, right):
+def recursion_slice(left, right):
+    global list_of_fence
 
     if left == right:
         return list_of_fence[left]
 
     mid_idx = (left + right) // 2
 
-    result = max(computed_max(list_of_fence, left, mid_idx), computed_max(list_of_fence, mid_idx + 1, right))
-    lo, hi = mid_idx, mid_idx + 1
-    height = min(list_of_fence[lo], list_of_fence[hi])
-    result = max(result, height * 2)
+    recursion_max = max(recursion_slice(left, mid_idx), recursion_slice(mid_idx + 1, right))
 
-    while left < lo or hi < right:
-        if hi < right and (lo == left or list_of_fence[lo - 1] < list_of_fence[hi + 1]):
-            hi += 1
-            height = min(height, list_of_fence[hi])
-        else:
-            lo -= 1
-            height = min(height, list_of_fence[lo])
-
-        result = max(result, height * (hi - lo + 1))
-
-    return result
+    height = min(list_of_fence[mid_idx], list_of_fence[mid_idx + 1])
+    mid_max = height
+    left_idx, right_idx = mid_idx, mid_idx + 1
+    while left < left_idx and right_idx < right:
+        if right_idx < right:
+            if list_of_fence[right_idx + 1] >= list_of_fence[left_idx - 1]:
+                right_idx += 1
+                height = min(height, list_of_fence[right_idx])
+                mid_max = max(mid_max, height * (right_idx + 1 - left_idx))
+                continue
+        left_idx -= 1
+        height = min(height, list_of_fence[left_idx])
+        mid_max = max(mid_max, height * (right_idx + 1 - left_idx))
+    return max(recursion_max, mid_max)
 
 
 C = int(stdin.readline())  # test case 의 개수
 
 for i in range(C):
     N = int(stdin.readline())  # 판자의 개수
+    global list_of_fence
     list_of_fence = list(map(int, stdin.readline().split()))
-    print(computed_max(list_of_fence, 0, N - 1))
+    print(recursion_slice(0, N - 1))
